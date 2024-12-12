201b60ea715d8aea26681e4a06a493c35e14a314
zR <2448370773@qq.com>
2024-12-12 12:29:06 +0800

    timeout set

--- baseline/sample/utils.py
+++ baseline/sample/utils.py
@@ -2,7 +2,7 @@ from zhipuai import ZhipuAI
 
 
 def call_large_model(messages, api_key, model="glm-4-plus", max_tokens=1024, temperature=0.0):
-    client = ZhipuAI(api_key=api_key, timeout=10000)
+    client = ZhipuAI(api_key=api_key)
     response = client.chat.completions.create(
         model=model,
         messages=messages,
